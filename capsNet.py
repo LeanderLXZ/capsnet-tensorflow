c15207f9749fd116a119e865aacd1b8d0c1fe7ea
Lu Xuzheng <466532329@qq.com>
2018-01-03 01:55:00 +0800

    Fix bug of shape

--- capsNet.py
+++ capsNet.py
@@ -227,7 +227,7 @@ class CapsNet(object):
         # Using convolution layers
         elif cfg.DECODER_TYPE == 'CONV':
             decoder_layers[0] = \
-                tf.reshape(tensor, (cfg.BATCH_SIZE, -1, tensor.get_shape().as_list()[1], 1, 1), name='reshape')
+                tf.reshape(tensor, (cfg.BATCH_SIZE, -1, 1, 1), name='reshape')
             for iter_conv, decoder_param in enumerate(cfg.DECODER_PARAMS):
                 with tf.variable_scope('decoder_{}'.format(iter_conv)):
                     # decoder_param:
@@ -241,7 +241,7 @@ class CapsNet(object):
         # Using transpose convolution layers
         elif cfg.DECODER_TYPE == 'CONV_T':
             decoder_layers[0] = \
-                tf.reshape(tensor, (cfg.BATCH_SIZE, -1, tensor.get_shape().as_list()[1], 1, 1), name='reshape')
+                tf.reshape(tensor, (cfg.BATCH_SIZE, -1, 1, 1), name='reshape')
             for iter_conv, decoder_param in enumerate(cfg.DECODER_PARAMS):
                 with tf.variable_scope('decoder_{}'.format(iter_conv)):
                     # decoder_param: {'kernel_size': None, 'stride': None, 'depth': None, 'padding': 'VALID'}
