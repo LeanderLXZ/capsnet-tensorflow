e2f61faf727b095862cc589687515e727c858fbb
Lu Xuzheng <466532329@qq.com>
2018-01-04 18:01:26 +0800

    Modify image saver

--- main.py
+++ main.py
@@ -283,8 +283,9 @@ class Main(object):
         # Combine images to grid image
         thin_gap = 1
         thick_gap = 3
-        new_im = Image.new(mode, ((rec_images.shape[1]+thin_gap+thick_gap)*save_row_size*2 - thick_gap,
-                                  (rec_images.shape[2]+thin_gap) * save_col_size - thin_gap), 'white')
+        avg_gap = (thin_gap+thick_gap)/2
+        new_im = Image.new(mode, (int((rec_images.shape[1]+avg_gap)*save_row_size*2 - thick_gap),
+                                  int((rec_images.shape[2]+thin_gap) * save_col_size - thin_gap)), 'white')
         for row_i in range(save_row_size*2):
             for col_i in range(save_col_size):
                 if (row_i+1) % 2 == 0:  # Odd
@@ -293,16 +294,16 @@ class Main(object):
                     else:
                         image = rec_images_in_square[(row_i+1)//2-1, col_i, :, :, :]
                     im = Image.fromarray(image, mode)
-                    new_im.paste(im, (col_i*(rec_images.shape[2]+thin_gap),
-                                      row_i*(rec_images.shape[1]+thin_gap)))
+                    new_im.paste(im, (int(col_i*(rec_images.shape[2]+thin_gap)),
+                                      int(row_i*rec_images.shape[1]+(row_i-1)*(thin_gap+thick_gap)/2 + thin_gap)))
                 else:  # Even
                     if mode == 'L':
                         image = real_images_in_square[int((row_i+1)//2), col_i, :, :]
                     else:
                         image = real_images_in_square[int((row_i+1)//2), col_i, :, :, :]
                     im = Image.fromarray(image, mode)
-                    new_im.paste(im, (col_i*(rec_images.shape[2]+thin_gap),
-                                      row_i*(rec_images.shape[1]+thick_gap)))
+                    new_im.paste(im, (int(col_i*(rec_images.shape[2]+thin_gap)),
+                                      int(row_i*(rec_images.shape[1]+(thin_gap+thick_gap)/2))))
 
         if epoch_i is None:
             save_image_path = os.path.join(img_path, 'batch_{}.jpg'.format(batch_counter))
